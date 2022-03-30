54a5f5f17099a4aa7bb9ca606a1a72220b691612
IyusDj <iyusdjruswana@gmail.com>
2022-03-30 22:26:49 +0700

    Setup babel, add assets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import hello from './generateModule'
 import './styles/main.scss'
+import pablo from './assets/pablo-finance.PNG'
+
+const imgs = document.getElementById('imgs')
+imgs.src = pablo
 
 console.log(hello)
